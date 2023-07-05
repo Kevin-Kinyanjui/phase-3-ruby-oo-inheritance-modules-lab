10a0fd56244f446efcde0487934ba68af38d6596
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-07-05 18:49:11 +0300

    first commit

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,5 @@
-require 'pry'
+require_relative './concerns/memorable'
+require_relative './concerns/paramable'
 
 class Artist
   attr_accessor :name
@@ -6,37 +7,20 @@ class Artist
 
   @@artists = []
 
-  def initialize
-    @@artists << self
-    @songs = []
-  end
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
+  def initialize
+    @songs = []
   end
 
   def self.all
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
   def add_song(song)
     @songs << song
     song.artist = self
-  end
-
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 end
